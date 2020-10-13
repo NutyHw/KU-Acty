c4b74c6c93cda8a354cc11529bb5b2b2e9ede055
rujipasnai <rujipasnai@hotmail.com>
2020-10-13 21:40:47 +0700

    added feed pages with header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import './index.css';
 import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
-import { Login } from './pages/login/login'
+import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
-import { RegisterCont } from './pages/register/registercont'
+import { RegisterCont } from './pages/register/registercont';
+import { NisitFeed } from './pages/feed/nisitfeed';
+import { OrgFeed } from './pages/feed/orgfeed';
 const App = () => {
   return(
       <div>
       <Route exact path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="/registercont" component={RegisterCont} />
+      <Route path="/nisitfeed" component={NisitFeed} />
+      <Route path="/orgfeed" component={OrgFeed} />
       </div>
   );
 };
