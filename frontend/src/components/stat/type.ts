8678781980d007d1f5508f58337f12f6951dc9ad
mrscotttt <52620083+mrscotttt@users.noreply.github.com>
2020-10-29 19:29:35 +0700

    Add files via upload

---
+++ frontend/src/components/stat/type.ts
@@ -0,0 +1,20 @@
+export type Event= {
+    event_name : string
+    organizer_id : string
+    benefit_hour : number
+    place : string,
+    event_start_date : string
+    event_end_date : string
+    contact : string
+    description : string
+    status : string
+    event_type : string
+    //++
+    hours : number
+    viewcount : number
+    viewfollow : number
+    whosfollower : string
+}
+
+
+
